3c0577236557ee36f4d63bd7bf6a580bdb200a63
root <jorlefgithub-47d1s1x5c478@outlook.com>
2022-02-19 08:27:17 +0100

    added file to manage errors

---
+++ backend/utils/errorsManager.js
@@ -0,0 +1,16 @@
+module.exports.signupErrors = (err, password) => {
+    let errors = {};
+    let errorsLength = err.errors.length;
+  
+    for (let i = 0; i < errorsLength; i++) {
+      if (err.errors[i].path.includes("first_name")) errors.first_name = "2 à 32 lettres";
+      if (err.errors[i].path.includes("last_name")) errors.last_name = "2 à 32 lettres";
+      if (err.errors[i].message.includes("email" && "unique")) errors.email = "le compte existe déjà";
+      if (err.errors[i].message.includes("email" && "formatted")) errors.email = "email non valide";
+    }
+  
+    if (password == false) errors.password = "8 à 32 caractères";
+  
+    return errors;
+  };
+  
